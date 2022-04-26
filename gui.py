d5e1804f35516dd79f1883a344133da87d02c785
Isaac <ihershenson@hmc.edu>
2022-04-25 18:38:25 -0700

    suggested autocomplete

--- gui.py
+++ gui.py
@@ -4,6 +4,16 @@ from tkinter import ttk
 from proof import *
 from tkinter.scrolledtext import ScrolledText
 import fitz
+from ttkwidgets.autocomplete import AutocompleteCombobox
+
+
+proof_methods = ['MultElem', 'accessAssumption', 'andElim', 'breakPower', 'cancelLeft', 
+'cancelRight', 'closure', 'combinePower', 'concludeSubproof', 'forAllIntroduction', 'forallElim',
+'identleft', 'identright', 'impliesIntroduction', 'introAssumption', 'introCases', 'introElement',
+'introGroup', 'introReflexive', 'introSubproof', 'inverseElimLHS', 'inverseElimRHS', 'leftMult',
+'leftSidesEq', 'modus', 'notElim', 'qed', 'reduceLogic', 'rightMult', 'rightSidesEq', 'show',
+'showReturn', 'splitPowerAddition', 'splitPowerMult', 'substituteLHS', 'substituteRHS',
+'switchSidesOfEqual', 'thereexistsElim', 'undo', 'writeLaTeXfile']
 
 class PDFViewer(ScrolledText):
     def show(self, pdf_file):
@@ -53,9 +63,69 @@ pdf1 = PDFViewer(mainframe, width=80, height=30, spacing3=5, bg='blue')
 pdf1.grid(row=5, column=1, sticky='nsew')
 pdf1.show('Simple Abelian Proof.pdf')
 
+
+def match_string():
+    hits = []
+    got = entry_bar.get()
+    for item in proof_methods:
+        if item.startswith(got):
+            hits.append(item)
+    return hits   
+
+def match_string_del():
+    hits = []
+    got = entry_bar.get()[:-1]
+    for item in proof_methods:
+        if item.startswith(got):
+            hits.append(item)
+    return hits 
+
+def check_empty(event):
+    if len(entry_bar.get()) == 1:
+        suggestions.set("") 
+    else:
+        hits = match_string_del()
+        show_hit(hits)
+
+def get_typed(event):
+    if len(event.keysym) == 1:
+        hits = match_string()
+        show_hit(hits)
+
+def show_hit(lst):
+    suggestions.set(lst)
+    if len(lst) == 1:
+        entry.set(lst[0])
+        detect_pressed.filled = True
+        entry_bar.icursor(END)
+
+def detect_pressed(event):    
+    key = event.keysym
+    if len(key) == 1 and detect_pressed.filled is True:
+        pos = entry_bar.index(INSERT)
+        entry_bar.delete(pos, END)
+
+detect_pressed.filled = False
+
+
 entry = StringVar()
-entry_bar = ttk.Entry(mainframe, width=50, textvariable=entry)
+entry_bar = Entry(
+    mainframe, 
+    width = 50,
+    bg='black',
+    insertbackground='white',
+    fg='white',
+    textvariable=entry)
 entry_bar.grid(column=1, row=3, sticky=(W, E))
+entry_bar.focus_set()
+entry_bar.bind('<KeyRelease>', get_typed)
+entry_bar.bind('<Key>', detect_pressed)
+entry_bar.bind('<BackSpace>', check_empty)
+
+
+
+#entry_bar = AutocompleteCombobox(mainframe, width=50, textvariable=entry, completevalues = proof_methods)
+
 
 showing = StringVar()
 showing.set("Proof : Simple Abelian Proof\n--------------------------------")
@@ -65,6 +135,9 @@ showProof = ttk.Label(mainframe, background="white", textvariable=showing).grid(
 ttk.Button(mainframe, text="Enter", command=enter).grid(column=3, row=3, sticky=W)
 
 ttk.Button(mainframe, text="Generate Latex", command=generateLaTeX).grid(column=3, row=4, sticky=W)
+
+suggestions = StringVar()
+showSuggestions = ttk.Label(mainframe, width = 50, background="white", textvariable=suggestions).grid(column=1, row=4, sticky=(W, E))
 
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
